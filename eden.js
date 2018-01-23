00788099cb29df7abf58cd42e50a0453a2d2012b
Justin R. Donnaruma <justin@donnaruma.us>
2018-01-23 13:13:21 -0500

    Add NewRelic APM tracking

--- eden.js
+++ eden.js
@@ -1,6 +1,7 @@
 // Simulate config options from your production environment by
 // customising the .env file in your project's root folder.
 require('dotenv').config();
+require('newrelic');
 
 // Require eden
 var eden = require('edencms');
