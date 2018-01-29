d3feb664131afe11773ca96eea1232bf611c19aa
Justin R. Donnaruma <justin@donnaruma.us>
2018-01-29 16:26:37 -0500

    filter out everything but new payments

--- routes/api/coinbase.js
+++ routes/api/coinbase.js
@@ -20,74 +20,82 @@ exports.post = function(req, res) {
 	console.log(typeof(data));
 	client.verifyCallback(data, req.headers['cb-signature']);
 	*/
-	if (client.verifyCallback(data, req.headers['cb-signature'])) { 
-		console.log(body);
-		User.model.findOne().where({
-			$or: [
-				{btcAddress: body.data.address},
-				{bchAddress: body.data.address},
-				{ethAddress: body.data.address},
-				{ltcAddress: body.data.address},
-			]
-		}).exec(function(err, user) {
-			var referred = false;
-			if (err) {
-				console.log("user: " + err);
-				res.apiResponse({
-					success: false,
-					error: "error"
-				});
-			}
-			if(user) {
-				var investment = new Investment.model({
-					investor: user._id,
-					address: body.data.address,
-					transID: body.additional_data.transaction.id,
-					confirmed: true,
-					createdAt: body.createdAt,
-					amount: parseFloat(body.additional_data.amount.amount),
-					currency: body.additional_data.amount.currency,
-					type: 'Investment',
-				});
-
-				investment.save(function(err) {
-					if (err) return res.apiError('error', err);
+	if (body.type === 'wallet:addresses:new-payment') {
+		if (client.verifyCallback(data, req.headers['cb-signature'])) {
+			console.log(body);
+			User.model.findOne().where({
+				$or: [
+					{btcAddress: body.data.address},
+					{bchAddress: body.data.address},
+					{ethAddress: body.data.address},
+					{ltcAddress: body.data.address},
+				]
+			}).exec(function(err, user) {
+				var referred = false;
+				if (err) {
+					console.log("user: " + err);
 					res.apiResponse({
-						success: true,
-						message: "investment created"
+						success: false,
+						error: "error"
 					});
-				});
-			} else {
-				res.apiResponse({
-					success: false,
-					error: "unknown user"
-				});
-			}
-			if(user.referrer !== undefined && user.referrer !== null && user.referrer !== '') {
-				var trunkAmount = parseFloat(body.additional_data.amount.amount) * parseFloat(user.referrerPercent);
-				trunkAmount = trunkAmount.toPrecision(9);
-				var investment = new Investment.model({
-					investor: user.referrer,
-					address: body.data.address,
-					transID: body.additional_data.transaction.id,
-					confirmed: true,
-					createdAt: body.createdAt,
-					amount: parseFloat(trunkAmount),
-					currency: body.additional_data.amount.currency,
-					type: 'Referral Bonus',
-				});
+				}
+				if(user) {
+					var investment = new Investment.model({
+						investor: user._id,
+						address: body.data.address,
+						transID: body.additional_data.transaction.id,
+						confirmed: true,
+						createdAt: body.createdAt,
+						amount: parseFloat(body.additional_data.amount.amount),
+						currency: body.additional_data.amount.currency,
+						type: 'Investment',
+					});
 
-				investment.save(function(err) {
-					if (err) {
-						console.log(err);
-					}
-				});	
-			}
-		});
+					investment.save(function(err) {
+						if (err) return res.apiError('error', err);
+						res.apiResponse({
+							success: true,
+							message: "investment created"
+						});
+					});
+				} else {
+					res.apiResponse({
+						success: false,
+						error: "unknown user"
+					});
+				}
+				if(user.referrer !== undefined && user.referrer !== null && user.referrer !== '') {
+					var trunkAmount = parseFloat(body.additional_data.amount.amount) * parseFloat(user.referrerPercent);
+					trunkAmount = trunkAmount.toPrecision(9);
+					var investment = new Investment.model({
+						investor: user.referrer,
+						address: body.data.address,
+						transID: body.additional_data.transaction.id,
+						confirmed: true,
+						createdAt: body.createdAt,
+						amount: parseFloat(trunkAmount),
+						currency: body.additional_data.amount.currency,
+						type: 'Referral Bonus',
+					});
+
+					investment.save(function(err) {
+						if (err) {
+							console.log(err);
+						}
+					});
+				}
+			});
+		} else {
+			res.apiResponse({
+				success: false,
+				error: "unauthorized user"
+			});
+		}
 	} else {
 		res.apiResponse({
 			success: false,
-			error: "unauthorized user"
+			error: "unnecessary transaction"
 		});
 	}
+
 };
